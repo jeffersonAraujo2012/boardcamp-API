0b7e41fe5b40abbd56727fedb06ecb9fa1666bbd
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-02-10 18:43:59 -0300

    Game schema updated to allow stockTotal and pricePerDay strictly greater than 0.

--- src/schemas/games.schema.js
+++ src/schemas/games.schema.js
@@ -3,8 +3,8 @@ import Joi from 'joi';
 const gameSchema = Joi.object({
   name: Joi.string().required(),
   image: Joi.string().required(),
-  stockTotal: Joi.number().min(0).required(),
-  pricePerDay: Joi.number().min(0).required(),
+  stockTotal: Joi.number().greater(0).required(),
+  pricePerDay: Joi.number().greater(0).required(),
 });
 
 export default gameSchema;
